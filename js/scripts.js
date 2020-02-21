cc494b4d90409578f6b88edeb38514ee67f51e11
josey kinnaman and Tim Klaus <tim@>
2020-02-20 20:46:17 -0800

    add additional js conditionals.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
   $("form#survey").submit(function(){
 
-    var gonzoScore = parseInt($("#gonzo-anxiety").val());
     var animalScore = parseInt($("#animal-party").val());
+    var gonzoScore = parseInt($("#gonzo-anxiety").val());
+    var fozzieScore = parseInt($("#fozzie-funny").val());
+    var piggyScore = parseInt($("#piggy-sassy").val());
+
+
+    console.log("Your current Gonzo Score is:  " + gonzoScore);
+    console.log("Your current Animal Score is:  " + animalScore);
+    console.log("Your current Fozzie score is:  " + fozzieScore);
+    console.log("Your current Ms. Piggy score is:  " + piggyScore);
+
+    if animalScore > fozzieScore && animalScore > gonzoScore && animalScore > piggyScore
     
-    console.log(gonzoScore);
-    console.log(animalScore);
     event.preventDefault();
   });
 });
